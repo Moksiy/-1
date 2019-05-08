c2e6916528f01c991b1a890f38dc0a8d68e8ee1b
Moksiy <MaksAligin@yandex.ru>
2019-05-08 14:55:25 +0300

    ЕПТВАЮ МАТЬ ГОТОВО

--- Практическая работа №1/MainWindow.xaml.cs
+++ Практическая работа №1/MainWindow.xaml.cs
@@ -37,10 +37,7 @@ namespace Практическая_работа__1
             InitializeComponent();
         }
 
-        private void X1_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
+        
 
         //Главная конпка РАССЧИТАТЬ
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -67,29 +64,29 @@ namespace Практическая_работа__1
         private bool TryParseX()
         {
             bool res = true;
-            int x1, x2, x3, x4, x5, x6, x7, x8, x9 = 0;
+            double x1, x2, x3, x4, x5, x6, x7, x8, x9 = 0;
             bool x1b, x2b, x3b, x4b, x5b, x6b, x7b, x8b, x9b = false;
-            x1b = int.TryParse(X1.Text, out x1); x2b = int.TryParse(X2.Text, out x2);
-            x3b = int.TryParse(X3.Text, out x3); x4b = int.TryParse(X4.Text, out x4);
-            x5b = int.TryParse(X5.Text, out x5); x6b = int.TryParse(X6.Text, out x6);
-            x7b = int.TryParse(X7.Text, out x7); x8b = int.TryParse(X8.Text, out x8);
-            x9b = int.TryParse(X9.Text, out x9);
+            x1b = double.TryParse(X1.Text, out x1); x2b = double.TryParse(X2.Text, out x2);
+            x3b = double.TryParse(X3.Text, out x3); x4b = double.TryParse(X4.Text, out x4);
+            x5b = double.TryParse(X5.Text, out x5); x6b = double.TryParse(X6.Text, out x6);
+            x7b = double.TryParse(X7.Text, out x7); x8b = double.TryParse(X8.Text, out x8);
+            x9b = double.TryParse(X9.Text, out x9);
             if (x1b && x2b && x3b && x4b && x5b && x6b && x7b && x8b && x9b) { res = true; } else { res = false; }
             return res;
         }
 
         private double[] Calculation()
         {
-            int[] x = new int[9];
-            x[0] = int.Parse(X1.Text);
-            x[1] = int.Parse(X2.Text);
-            x[2] = int.Parse(X3.Text);
-            x[3] = int.Parse(X4.Text);
-            x[4] = int.Parse(X5.Text);
-            x[5] = int.Parse(X6.Text);
-            x[6] = int.Parse(X7.Text);
-            x[7] = int.Parse(X8.Text);
-            x[8] = int.Parse(X9.Text);
+            double[] x = new double[9];
+            x[0] = double.Parse(X1.Text);
+            x[1] = double.Parse(X2.Text);
+            x[2] = double.Parse(X3.Text);
+            x[3] = double.Parse(X4.Text);
+            x[4] = double.Parse(X5.Text);
+            x[5] = double.Parse(X6.Text);
+            x[6] = double.Parse(X7.Text);
+            x[7] = double.Parse(X8.Text);
+            x[8] = double.Parse(X9.Text);
             double[] inv = new double[9];
             double[] eigenvalue = new double[4];
             eigenvalue = CharacterOfMatrix(x);
@@ -100,21 +97,21 @@ namespace Практическая_работа__1
         }
 
         //Нахождение характеристического многочлена матрицы и нахождение его корней 
-        private double[] CharacterOfMatrix(int[] matrix)
+        private double[] CharacterOfMatrix(double[] matrix)
         {
             double[] resCharacter = new double[4];
             //Коэффициенты для кубического уравнения
-            int a, b, c;
+            double a, b, c;
             a = -1;
             b = matrix[0] + matrix[4] + matrix[8];
             c = -((matrix[4] * matrix[8]) + (matrix[0] * matrix[8]) + (matrix[0] * matrix[4]) - (matrix[5] * matrix[7]) - (matrix[1] * matrix[3]) - (matrix[2] * matrix[6]));
-            int det = Determinant(matrix);
+            double det = Determinant(matrix);
             resCharacter = CubicEquation(a, b, c, det);
             return resCharacter;
         }
 
         //Нахождение корней кубического уравнения
-        private double[] CubicEquation(int a, int b, int c, int d)
+        private double[] CubicEquation(double a, double b, double c, double d)
         {
             double aa, bb, cc;
             aa = b / a; bb = c / a; cc = d / a;
@@ -135,7 +132,7 @@ namespace Практическая_работа__1
                 if (Q > 0)
                 {
                     X = Math.Abs(R) / Math.Sqrt(Math.Pow(Q, 3));
-                    FI = (Math.Log10(X + Math.Sqrt(X * X - 1))) / 3;
+                    FI = (Math.Log(X + Math.Sqrt(X * X - 1))) / 3;
                     res[0] = (-2 * Math.Sign(R) * Math.Sqrt(Q) * Math.Cosh(FI)) - aa / 3;
                     res[1] = Math.Sign(R)*Math.Sqrt(Q)*Math.Cosh(FI) - aa/3;
                     res[2] = Math.Sqrt(3)*Math.Sqrt(Q)*Math.Sinh(FI);
@@ -144,7 +141,7 @@ namespace Практическая_работа__1
                 else if (Q < 0)
                 {
                     X = Math.Abs(R) / Math.Sqrt(Math.Pow(Math.Abs(Q), 3));
-                    FI = (Math.Log10(X + Math.Sqrt(X * X + 1))) / 3;
+                    FI = (Math.Log(X + Math.Sqrt(X * X + 1))) / 3;
                     res[0] = (-2 * Math.Sign(R) * Math.Sqrt(Math.Abs(Q)) * Math.Sinh(FI)) - aa / 3;
                     res[1] = Math.Sign(R)*Math.Sqrt(Math.Abs(Q))*Math.Sinh(FI) - aa/3;
                     res[2] = Math.Sqrt(3)*Math.Sqrt(Math.Abs(Q))*Math.Cosh(FI);
@@ -160,20 +157,20 @@ namespace Практическая_работа__1
             }
             else // S==0
             {
-                res[0] = -2 * Math.Sign(R) * Math.Sqrt(Q) - (aa / 3);
-                res[1] = Math.Sign(R) * Math.Sqrt(Q) - (aa / 3);
+                res[0] = -2 * Signum(R) * Math.Sqrt(Q) - (aa / 3);
+                res[1] = Signum(R) * Math.Sqrt(Q) - (aa / 3);
                 res[2] = res[1];
             }
-            res[0] = Math.Round(res[0]);
-            res[1] = Math.Round(res[1]);
-            res[2] = Math.Round(res[2]);
+            //res[0] = Math.Round(res[0]);
+            //res[1] = Math.Round(res[1]);
+            //res[2] = Math.Round(res[2]);
             return res;
         }
 
         //Нахождение определителя матрицы
-        private int Determinant(int[] mat)
+        private double Determinant(double[] mat)
         {
-            int determinant;
+            double determinant;
 
             determinant = ((mat[0] * mat[4] * mat[8]) + (mat[6] * mat[1] * mat[5]) + (mat[2] * mat[3] * mat[7])
                 - (mat[6] * mat[4] * mat[2]) - (mat[3] * mat[1] * mat[8]) - (mat[7] * mat[5] * mat[0]));
@@ -184,21 +181,33 @@ namespace Практическая_работа__1
         //Вывод
         private void PrintResult(double[] res)
         {
-            string s1 = Convert.ToString(res[4]) + " + i" + Convert.ToString(res[8]);
-            string s2 = Convert.ToString(res[4]) + " - i" + Convert.ToString(res[8]);
-            //Thread.Sleep(500);
-            X1.Text = Convert.ToString(res[0]);
-            X2.Text = Convert.ToString(res[1]);
-            X3.Text = Convert.ToString(res[2]);            
-            X4.Text = Convert.ToString(res[3]);
-            if (res[9] == 1) { X5.Text = s1; } else { X5.Text = Convert.ToString(res[4]); }
-            X6.Text = Convert.ToString(res[5]);
-            X7.Text = Convert.ToString(res[6]);
-            X8.Text = Convert.ToString(res[7]);
-            if (res[9] == 1) { X9.Text = s2; } else { X9.Text = Convert.ToString(res[8]); }
+            bool result = false;
+            foreach(var r in res)
+            {
+                if (double.IsNaN(r) || double.IsInfinity(r))
+                {
+                    result = true;
+                }
+            }
+            if (result == true) { MessageBox.Show("Переполнение"); }
+            else
+            {
+                string s1 = Convert.ToString(res[4]) + " + i" + Convert.ToString(res[8]);
+                string s2 = Convert.ToString(res[4]) + " - i" + Convert.ToString(res[8]);
+                //Thread.Sleep(500);
+                X1.Text = Convert.ToString(res[0]);
+                X2.Text = Convert.ToString(res[1]);
+                X3.Text = Convert.ToString(res[2]);
+                X4.Text = Convert.ToString(res[3]);
+                if (res[9] == 1) { X5.Text = s1; } else { X5.Text = Convert.ToString(res[4]); }
+                X6.Text = Convert.ToString(res[5]);
+                X7.Text = Convert.ToString(res[6]);
+                X8.Text = Convert.ToString(res[7]);
+                if (res[9] == 1) { X9.Text = s2; } else { X9.Text = Convert.ToString(res[8]); }
+            }
         }
 
-        private double[] st(double[] eigen, int[] matrix)
+        private double[] st(double[] eigen, double[] matrix)
         {
             double[] res = new double[10];
 
@@ -218,9 +227,9 @@ namespace Практическая_работа__1
         }
 
         //Составление характеристической матрицы
-        private int[] CharacterMatrix(int[] matrix, double[] eigenval)
+        private double[] CharacterMatrix(double[] matrix, double[] eigenval)
         {
-            int[] res = new int[10];
+            double[] res = new double[10];
             res[0] = matrix[0] - (int)eigenval[0];
             res[1] = matrix[1];
             res[2] = matrix[2];
@@ -252,16 +261,16 @@ namespace Практическая_работа__1
         }
 
         //Собственные значения равны
-        private double[] Answer2(double[] x, int[] matrix)
+        private double[] Answer2(double[] x, double[] matrix)
         {
-            int rank = Rank(matrix);
+            double rank = Rank(matrix);
             double[] answer = new double[10];
             answer[0] = x[0];
-            if (rank == 1) { answer[1] = 1; } else if (rank == 2) { answer[1] = 1; } else { answer[1] = 0; }
+            if (rank == 1) { answer[1] = 1; } else if (rank == 2 || rank == 3) { answer[1] = 1; } else { answer[1] = 0; }
             answer[2] = 0;
             answer[3] = 0;
             answer[4] = x[1];
-            if (rank == 1) { answer[5] = 0; } else if (rank == 2) { answer[5] = 1; } else { answer[5] = 0; }
+            if (rank == 1) { answer[5] = 0; } else if (rank == 2 || rank == 3) { answer[5] = 1; } else { answer[5] = 0; }
             answer[6] = 0;
             answer[7] = 0;
             answer[8] = x[2];
@@ -270,14 +279,14 @@ namespace Практическая_работа__1
         }
 
         //Два разных значения
-        private double[] Answer3(double[] x, int[] matrix)
+        private double[] Answer3(double[] x, double[] matrix)
         {
             double s = 0;
             if (x[1] == x[2]) { s = x[0]; x[2] = s; x[0] = x[1]; }
             double[] answer = new double[10];
-            int rank = Rank(matrix);
+            double rank = Rank(matrix);
             answer[0] = x[0];
-            if (rank != 2 && x[0] == x[1] && matrix[9] != 1) { answer[1] = 1; } else { answer[1] = 0; }
+            if (rank != 2 && x[0] == x[1] && matrix[9] != 1) { if (rank == 0) { answer[1] = 0; } else { answer[1] = 1; } } else { answer[1] = 0; }
             answer[2] = 0;
             answer[3] = 0;
             answer[4] = x[1]; 
@@ -290,16 +299,16 @@ namespace Практическая_работа__1
         }
 
         //Подсчет ранга матрицы
-        private int Rank(int[] matrix)
+        private double Rank(double[] matrix)
         {
-            int answer = 0;
+            double answer = 0;
             if (Determinant(matrix) == 0)
             {
-                int[] m = new int[9]; m = Minors(matrix);
+                double[] m = new double[9]; m = Minors(matrix);
                 if (m[0] == m[1] && m[1] == m[2] && m[2] == m[3] && m[3] == m[4] && m[4] == m[5] && m[5] == m[6] && m[6] == m[7] && m[7] == m[8])
                 {
                     answer = 1;
-
+                    if (m[0] == 0 && m[0] == m[1] && m[1] == m[2] && m[2] == m[3] && m[3] == m[4] && m[4] == m[5] && m[5] == m[6] && m[6] == m[7] && m[7] == m[8]) { answer = 0; }
                 }
                 else { answer = 2; }
             }
@@ -308,9 +317,9 @@ namespace Практическая_работа__1
         }
 
         //Подсчет миноров матрицы
-        private int[] Minors(int[] matrix)
+        private double[] Minors(double[] matrix)
         {
-            int[] res = new int[9];
+            double[] res = new double[9];
             res[0] = (matrix[4] * matrix[8]) - (matrix[5] * matrix[7]);
             res[1] = (matrix[3] * matrix[8]) - (matrix[5] * matrix[6]);
             res[2] = (matrix[3] * matrix[7]) - (matrix[4] * matrix[6]);
@@ -321,6 +330,13 @@ namespace Практическая_работа__1
             res[7] = (matrix[0] * matrix[5]) - (matrix[3] * matrix[2]);
             res[8] = (matrix[0] * matrix[4]) - (matrix[1] * matrix[3]);
             return res;
+        }
+
+        private double Signum(double a)
+        {
+            double result;
+            if (a > 0) { result = 1; } else if(a < 0) { result = -1; } else { result = 0; }
+            return result;
         }
 
 
